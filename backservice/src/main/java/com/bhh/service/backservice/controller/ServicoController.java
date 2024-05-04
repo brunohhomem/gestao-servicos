1687a516ded67cb85a2ccac13d80ca11e8905c39
Bruno H. Homem <brunohhomem@gmail.com>
2024-05-04 07:18:17 -0300

    added crossorigin

--- backservice/src/main/java/com/bhh/service/backservice/controller/ServicoController.java
+++ backservice/src/main/java/com/bhh/service/backservice/controller/ServicoController.java
@@ -15,37 +15,44 @@ public class ServicoController {
     @Autowired private ServicoService service;
 
     @GetMapping
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarTodos(){
         return service.buscarTodos();
     }
 
     @GetMapping("/pendentes")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarPagamentoPendente(){
         return service.buscarPagamentoPendente();
     }
 
     @GetMapping("/cancelados")
+    @CrossOrigin("http://localhost:3000")
     public List<Servico> buscarServicoCancelado(){
         return service.buscarServicoCancelado();
     }
 
     @PostMapping
+    @CrossOrigin("http://localhost:3000")
     public Servico inserir(@RequestBody Servico servico){
         return service.inserir(servico);
     }
 
     @PutMapping
+    @CrossOrigin("http://localhost:3000")
     public Servico alterar(@RequestBody Servico servico){
         return service.alterar(servico);
     }
 
     @PostMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> cancelar(@PathVariable Long id){
         service.cancelarServico(id);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> remover(@PathVariable Long id){
         service.excluir(id);
         return ResponseEntity.ok().build();
